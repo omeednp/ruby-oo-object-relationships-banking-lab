51ac6b93900bb5ef61aacab409921bb536054acb
github username <scott.gloyna01@gmail.com>
2021-01-14 11:00:44 -0600

    We built a janky transfer process

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    @@all =[]
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        @@all << self
+    end
+
+    #def update_balance(value)
+       # @balance 
+    def deposit(value)
+        @balance+=value
+    end
+    
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.balance >0 and self.status != "closed"
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+
+    
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,52 @@
 class Transfer
   # your code here
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize (sender, receiver, amount)
+    @sender=sender
+    @receiver=receiver
+    @amount=amount
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? and receiver.valid?
+  end
+
+  def funds_check(person, value)
+    person.balance >= value
+  end
+    
+  def execute_transaction
+    if @status == "pending"
+      if funds_check(@sender,@amount) && @sender.valid? && @receiver.valid?
+        sender.balance -= @amount
+        receiver.balance += @amount
+        @status = "complete"
+      else
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+  end
+
+  def switch_accounts
+    account_holder1 = @sender
+    account_holder2 = @receiver
+    @sender = account_holder2
+    @receiver = account_holder1
+  end
+
+
+  def reverse_transfer
+    if @status == "complete"
+      switch_accounts
+      @status ="pending"
+      execute_transaction
+      @status = "reversed"
+    end
+  end
+
+
 end
